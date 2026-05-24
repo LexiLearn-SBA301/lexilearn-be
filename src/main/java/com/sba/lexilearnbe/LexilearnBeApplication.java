b5b13673bbab1a96805d3b065032b4881c291eac
tinacoding-me <tiennhat110505@gmail.com>
2026-05-24 12:53:11 +0700

    init structure of source code

---
+++ src/main/java/com/sba/lexilearnbe/LexilearnBeApplication.java
@@ -0,0 +1,13 @@
+package com.sba.lexilearnbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexilearnBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LexilearnBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sba/lexilearnbe/LexilearnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sba.lexilearnbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexilearnBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
