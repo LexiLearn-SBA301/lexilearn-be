db16e9e03a7124120456a168e6c89405f1ac3436
Phucdoan312 <phucdoan849@gmail.com>
2026-06-17 13:54:32 +0700

    [REFACTOR] use sequence endpoints for reordering

--- src/main/java/com/sba/lexilearnbe/modules/workdetail/controller/admin/AdminArtisticFeatureController.java
+++ src/main/java/com/sba/lexilearnbe/modules/workdetail/controller/admin/AdminArtisticFeatureController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -82,7 +81,7 @@ public class AdminArtisticFeatureController {
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/works/{workId}/artistic-features/order")
+    @PatchMapping("/works/{workId}/artistic-features/sequence")
     @Operation(summary = "Sắp xếp lại thứ tự đặc điểm nghệ thuật")
     public ResponseEntity<ApiResponse<List<ArtisticFeatureResponse>>> reorderArtisticFeatures(
             @PathVariable UUID workId,
--- src/main/java/com/sba/lexilearnbe/modules/workdetail/controller/admin/AdminWorkCharacterController.java
+++ src/main/java/com/sba/lexilearnbe/modules/workdetail/controller/admin/AdminWorkCharacterController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -82,7 +81,7 @@ public class AdminWorkCharacterController {
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/works/{workId}/characters/order")
+    @PatchMapping("/works/{workId}/characters/sequence")
     @Operation(summary = "Sắp xếp lại thứ tự nhân vật")
     public ResponseEntity<ApiResponse<List<WorkCharacterResponse>>> reorderCharacters(
             @PathVariable UUID workId,
