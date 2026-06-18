cdec8f4815d283dce71c93f10c345f71edb2d93b
HuuNhat <thuunhat@gmail.com>
2026-06-18 08:47:31 +0700

    feat: forgot password

--- src/main/java/com/sba/lexilearnbe/shared/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/sba/lexilearnbe/shared/infrastructure/security/SecurityConfig.java
@@ -28,6 +28,8 @@ public class SecurityConfig {
             "/api/v1/auth/register",
             "/api/v1/auth/verify-otp",
             "/api/v1/auth/resend-otp",
+            "/api/v1/auth/forgot-password",
+            "/api/v1/auth/reset-password",
             "/api/v1/auth/login",
             "/api/v1/auth/refresh",
             "/api/v1/auth/logout",
